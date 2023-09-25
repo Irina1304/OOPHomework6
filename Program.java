bb72ad47ab3b4fbd8b05b7d6901bf11074d8a040
Irina <migalina.i@icloud.com>
2023-09-26 00:14:20 +0300

    hw6

---
+++ Program.java
@@ -0,0 +1,12 @@
+package srp2;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Order order = new Order();
+        ConsoleReader consoleReader = new ConsoleReader(order);
+        consoleReader.inputFromConsole();
+        JsonFormat jsonFormat = new JsonFormat(order);
+        jsonFormat.saveToJson();
+    }
+}
